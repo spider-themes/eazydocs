b19d7d1a86968069c2ea5687a8151ba2dc4db2ef
Eh Jewel <ehjewel2@gmail.com>
2022-04-24 04:32:49 +0600

    settings output updated

--- templates/single-docs.php
+++ templates/single-docs.php
@@ -19,9 +19,9 @@ $credit_enable      = '1';
 $credit_text        = sprintf(__("%s", 'eazydocs'), 'Powered By <a href="https://wordpress.org/plugins/eazydocs/" target="_blank">EazyDocs</a>');
 if ( class_exists('EazyDocsPro') ) {
 	$cz_options = get_option( 'eazydocs_customizer' ); // prefix of framework
-	$layout     = $cz_options['docs-single-layout'] ?? '';
-    $doc_width          = $cz_options['docs-page-width'] ?? '';
-    $search_banner      = $cz_options['docs-search-banner'] ?? '';
+	$layout     = $cz_options['doc_layout']['docs_single_layout'] ?? '';
+    $doc_width          = $cz_options['doc_layout']['docs_page_width'] ?? '';
+    $search_banner      = $cz_options['doc_layout']['docs_search_banner'] ?? '';
     $doc_container      = $doc_width == 'full-width' ? 'container-fluid pl-60 pr-60' : 'container custom_container';
 	$content_wrapper    = $doc_width == 'full-width' ? 'doc_documentation_full_area' : '';
 
