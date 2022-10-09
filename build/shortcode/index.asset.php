3a798a3a486c4fcc9235cbb88da684ebf3067fb6
Eh Jewel <ehjewel2@gmail.com>
2022-10-09 15:01:41 +0600

    rest of block

--- build/shortcode/index.asset.php
+++ build/shortcode/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '19450727d3c742f15f85');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '88a4c0fbad3b7517b32d');
