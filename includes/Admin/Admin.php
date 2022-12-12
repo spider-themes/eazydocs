ff2f8cb0b154aecc45d27cee7e3cc0f79dee6bab
delweratjk <delweratjk@gmail.com>
2022-12-12 21:37:00 +0600

    Renamed admin menu

--- includes/Admin/Admin.php
+++ includes/Admin/Admin.php
@@ -86,12 +86,14 @@ class Admin
 
 
 		if ( function_exists('ezd_is_premium') && ezd_is_premium() == true ) {
-			$ezd_menu_title = __('Docs Builder', 'eazyDocs');
+			$ezd_menu_title = __('EazyDocs Pro', 'eazyDocs');
 		} else {
 			$ezd_menu_title = __('EazyDocs', 'eazyDocs');
 		}
 
 		add_menu_page(__($ezd_menu_title, 'eazyDocs'), __($ezd_menu_title, 'eazyDocs'), $capabilites, 'eazydocs', [$this, 'eazydocs_page'], 'dashicons-media-document', 10);
+		add_submenu_page('eazydocs', __('Docs Builder', 'eazydocs'), __('Docs Builder', 'eazydocs'), $capabilites, 'eazydocs');
+
 
 		if ( ezd_is_premium() ) {
 			if (in_array($current_user, $cz_roled)) {
