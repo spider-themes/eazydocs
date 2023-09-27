2c89490d0b2af11e046e09ad29f2ac3e607df2b9
Rony015 <53683822+Rony015@users.noreply.github.com>
2023-09-27 15:52:32 +0600

    some code update

--- includes/Elementor/Docs/Doc_Widget.php
+++ includes/Elementor/Docs/Doc_Widget.php
@@ -898,19 +898,18 @@ class Doc_Widget extends Widget_Base {
             });
         }
 
-    });
-
-
-    // custom tab js
-    $('.tab-menu li a').on('click', function() {
-        var target = $(this).attr('data-rel');
-        $('.tab-menu li a').removeClass('active');
-        $(this).addClass('active');
-        $('#' + target)
-            .fadeIn('slow')
-            .siblings('.tab-box')
-            .hide();
-        return false;
+        // custom tab js
+        $('.tab-menu li a').on('click', function(e) {
+            e.preventDefault();
+            var target = $(this).attr('data-rel');
+            $('.tab-menu li a').removeClass('active');
+            $(this).addClass('active');
+            $('#' + target)
+                .fadeIn('slow')
+                .siblings('.tab-box')
+                .hide();
+            return false;
+        });
     });
 
 })(jQuery);
