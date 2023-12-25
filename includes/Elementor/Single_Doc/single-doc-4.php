603c4c0eaed735f7e83c7a2a7bcc6730d33c0afc
Delwer Hossain <delweratjk@gmail.com>
2023-12-25 14:26:01 +0600

    Single doc widget preset 4 show more button issue solved

--- includes/Elementor/Single_Doc/single-doc-4.php
+++ includes/Elementor/Single_Doc/single-doc-4.php
@@ -2,22 +2,22 @@
     <?php
 	foreach ( $sections as $section ) :
 		?>
-    <a href="<?php echo get_permalink( $section->ID ) ?>">
-        <div class="h_item">
-            <?php echo ! empty( $section->ID ) ? get_the_post_thumbnail( $section->ID, 'full' ) : ''; ?>
-            <h4 class="ct-heading-text"><?php echo wp_kses_post( $section->post_title ); ?></h4>
-            <div class="ct-content-text">
-                <?php
-						if ( strlen( trim( $section->post_excerpt ) ) != 0 ) {
-							echo wpautop( $section->post_excerpt );
-						} else {
-							echo wpautop( wp_trim_words( $section->post_content, $settings['doc_sec_excerpt'], '' ) );
-						}
-						?>
+        <a href="<?php echo get_permalink( $section->ID ) ?>">
+            <div class="h_item">
+                <?php echo ! empty( $section->ID ) ? get_the_post_thumbnail( $section->ID, 'full' ) : ''; ?>
+                <h4 class="ct-heading-text"><?php echo wp_kses_post( $section->post_title ); ?></h4>
+                <div class="ct-content-text">
+                    <?php
+                        if ( strlen( trim( $section->post_excerpt ) ) != 0 ) {
+                            echo wpautop( $section->post_excerpt );
+                        } else {
+                            echo wpautop( wp_trim_words( $section->post_content, $settings['doc_sec_excerpt'], '' ) );
+                        }
+                    ?>
+                </div>
             </div>
-        </div>
-    </a>
-    <?php
+        </a>
+        <?php
 	endforeach;
 	?>
 </div>
@@ -30,7 +30,7 @@ $sections2 = get_children( array(
 	'orderby'        => 'menu_order',
 	'order'          => $settings['order'],
 	'posts_per_page' => ! empty( $settings['ppp_sections2'] ) ? $settings['ppp_sections2'] : - 1,
-	'offset'         => ! empty( $settings['ppp_sections'] ) ? $settings['ppp_sections'] : 8,
+	'offset'         => ! empty( $settings['ppp_sections'] ) ? $settings['ppp_sections'] : 8
 ) );
 ?>
 <div class="h_content_items box-item collapse-wrap">
@@ -38,22 +38,22 @@ $sections2 = get_children( array(
         <?php
 		foreach ( $sections2 as $section ) :
 			?>
-        <a href="<?php echo get_permalink( $section->ID ) ?>">
-            <div class="h_item">
-                <?php echo ! empty( $section->ID ) ? get_the_post_thumbnail( $section->ID, 'full' ) : ''; ?>
-                <h4 class="ct-heading-text"><?php echo wp_kses_post( $section->post_title ); ?></h4>
-                <div class="ct-content-text">
-                    <?php
-							if ( strlen( trim( $section->post_excerpt ) ) != 0 ) {
-								echo wpautop( $section->post_excerpt );
-							} else {
-								echo wpautop( wp_trim_words( $section->post_content, $settings['doc_sec_excerpt'], '' ) );
-							}
-							?>
+            <a href="<?php echo get_permalink( $section->ID ) ?>">
+                <div class="h_item">
+                    <?php echo ! empty( $section->ID ) ? get_the_post_thumbnail( $section->ID, 'full' ) : ''; ?>
+                    <h4 class="ct-heading-text"><?php echo wp_kses_post( $section->post_title ); ?></h4>
+                    <div class="ct-content-text">
+                        <?php
+                            if ( strlen( trim( $section->post_excerpt ) ) != 0 ) {
+                                echo wpautop( $section->post_excerpt );
+                            } else {
+                                echo wpautop( wp_trim_words( $section->post_content, $settings['doc_sec_excerpt'], '' ) );
+                            }
+                        ?>
+                    </div>
                 </div>
-            </div>
-        </a>
-        <?php
+            </a>
+            <?php
 		endforeach;
 		?>
     </div>
@@ -62,34 +62,36 @@ $sections2 = get_children( array(
 <?php 
 if ( ! empty( $settings['show_more_btn'] ) ) : 
 	?>
-<div class="more text-center">
-    <a class="icon_btn2 blue collapse-btn" href="#">
-        <span>
-            <ion-icon name="caret-down-circle-outline"></ion-icon>
-            <?php echo wp_kses_post( $settings['show_more_btn'] ); ?>
-        </span>
-        <span>
-            <ion-icon name="caret-up-circle-outline"></ion-icon>
-            <?php echo wp_kses_post( $settings['show_less_btn'] ); ?>
-        </span>
-    </a>
-</div>
-<script type="text/javascript">
-;
-(function($) {
-    "use strict";
-
-    $(document).ready(function() {
-        if ($('.collapse-btn').length > 0) {
-            $('.collapse-btn').on('click', function(e) {
-                e.preventDefault();
-                $(this).toggleClass('active');
-                $('.collapse-wrap').slideToggle(500);
+    <div class="more text-center">
+        <a class="icon_btn2 blue collapse-btn" href="#">
+            <span>
+                <ion-icon name="caret-down-circle-outline"></ion-icon>
+                <?php echo wp_kses_post( $settings['show_more_btn'] ); ?>
+            </span>
+            <span>
+                <ion-icon name="caret-up-circle-outline"></ion-icon>
+                <?php echo wp_kses_post( $settings['show_less_btn'] ); ?>
+            </span>
+        </a>
+    </div>
+    <script type="text/javascript">
+        ;(function($) {
+            "use strict";
+            
+            $(document).ready(function() {
+                function general() {
+                    if ($('.collapse-btn').length > 0) {
+                        $('.collapse-btn').on('click', function(e) {
+                            e.preventDefault();
+                            $(this).toggleClass('active');
+                            $('.collapse-wrap').slideToggle(500);
+                        });
+                    }
+                }
+                general();
             });
-        }
-    });
 
-})(jQuery);
-</script>
-<?php 
+        })(jQuery);
+    </script>
+    <?php 
 endif;
