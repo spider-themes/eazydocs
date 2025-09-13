9e9a855df2199a276d4da9c9f20e6ff8ea85f02c
Delwer Hossain <delweratjk@gmail.com>
2025-09-13 17:30:49 +0600

    Updated the review notice design

--- includes/notices/asking-for-review.php
+++ includes/notices/asking-for-review.php
@@ -35,37 +35,55 @@ function ezd_notify_give_review() {
 	] );
 	?>
     <div class="notice notice-success is-dismissible" id="ezd_notify_review">
-        <h3><?php esc_html_e( 'Give EazyDocs a review', 'eazydocs' ); ?></h3>
-        <p>
-			<?php
-			if ( count( $docs ) <= 0 ) {
-				esc_html_e( 'Thank you for choosing EazyDocs. We hope you love it. Could you take a couple of seconds posting a nice review to share your happy experience?', 'eazydocs' );
-			} else {
-				$articles_count = count( $articles ) - count( $docs );
-				$articles_text  = $articles_count > 0 ? " and <b>" . $articles_count . "</b> articles" : '';
-				echo wp_kses(
-					sprintf(
-					/* translators: 1: number of docs created, 2: additional articles text */
-						__( 'You have created <b>%1$d</b> docs%2$s with EazyDocs. That\'s awesome! May we ask you to give it a 5-Star rating on WordPress? It will help us spread the word and boost our motivation.', 'eazydocs' ),
-						count( $docs ),
-						$articles_text
-					),
-					array(
-						'b'      => array(), // Allow <b> tag
-						'strong' => array(), // If needed, allow <strong> tag
-					)
-				);
-			}
-			?>
-        </p>
-        <p class="ezd_notify_review_subheading" style="margin-bottom: 20px;">
-			<?php esc_html_e( 'We will be forever grateful. Thank you in advance.', 'eazydocs' ); ?>
-        </p>
-        <p>
-            <a href="javascript:;" data="rateNow" class="button button-primary" style="margin-right: 5px"><?php esc_html_e( 'Rate now', 'eazydocs' ) ?></a>
-            <a href="javascript:;" data="later" class="button" style="margin-right: 5px"> <?php esc_html_e( 'Later', 'eazydocs' ) ?> </a>
-            <a href="javascript:;" data="alreadyDid" class="button"><?php esc_html_e( 'Already did', 'eazydocs' ) ?> </a>
-        </p>
+		<div>
+			<img src="<?php echo EAZYDOCS_IMG . '/eazydocs-logo.png' ?>" />
+		</div>
+		<div>
+			<h3><?php esc_html_e( 'Give EazyDocs a review', 'eazydocs' ); ?></h3>
+			<p style="margin-top: 10px;">
+				<?php
+				if ( count( $docs ) <= 0 ) {
+					esc_html_e( 'Thank you for choosing EazyDocs. We hope you love it. Could you take a couple of seconds posting a nice review to share your happy experience?', 'eazydocs' );
+				} else {
+					$articles_count = count( $articles ) - count( $docs );
+					$articles_text  = $articles_count > 0 ? " and <b>" . $articles_count . "</b> articles" : '';
+					echo wp_kses(
+						sprintf(
+						/* translators: 1: number of docs created, 2: additional articles text */
+							__( 'You have created <b>%1$d</b> docs%2$s with EazyDocs. That\'s awesome! May we ask you to give it a 5-Star rating on WordPress? It will help us spread the word and boost our motivation.', 'eazydocs' ),
+							count( $docs ),
+							$articles_text
+						),
+						array(
+							'b'      => array(), // Allow <b> tag
+							'strong' => array(), // If needed, allow <strong> tag
+						)
+					);
+				}
+				?>
+			</p>
+			<p class="ezd_notify_review_subheading" style="margin-bottom: 12px;">
+				<?php esc_html_e( 'We will be forever grateful. Thank you in advance.', 'eazydocs' ); ?>
+			</p>
+			<div class="action_links">
+				<a href="javascript:;" data="rateNow" class="button button-primary" style="margin-right: 5px">
+					<?php esc_html_e( 'Ok, you deserve', 'eazydocs' ) ?>
+					<span class='icon_star'></span>
+					<span class='icon_star'></span>
+					<span class='icon_star'></span>
+					<span class='icon_star'></span>
+					<span class='icon_star'></span>
+				</a>
+				<a href="javascript:;" class="btn" data="later" style="margin-right: 5px"> 
+					<span class='icon icon_clock_alt'></span>
+					<?php esc_html_e( 'Nope, maybe later', 'eazydocs' ) ?> 
+				</a>
+				<a href="javascript:;" class="btn red" data="alreadyDid">
+					<span class='icon icon_close_alt2'></span>
+					<?php esc_html_e( 'Never show again', 'eazydocs' ) ?> 
+				</a>
+			</div>
+		</div>
     </div>
 
     <script>
