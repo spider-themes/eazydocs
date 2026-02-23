898dde5998429fca97e190c3f177bb6e158fe101
Niloy Mahmud Apu <niloymahmudapu@gmail.com>
2026-02-23 10:16:52 +0600

    chore: remove build and vendor folders as they will be auto-generated

--- build/eazydocs-toolbar/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => 'e0a2aeef2f4569519e7d');
--- build/tabbed-docs/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'e195982bdf5d9d8f6520');
