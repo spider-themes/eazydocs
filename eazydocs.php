f48ca3f24bbd642e3e41f32ed0b5c79ad7fa19a6
Md Akram Hossain <50274249+akram73@users.noreply.github.com>
2026-05-25 16:52:10 +0600

    v2.11.3

--- eazydocs.php
+++ eazydocs.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://eazydocs.spider-themes.net
  * Author: spider-themes
  * Author URI: https://eazydocs.spider-themes.net
- * Version: 2.11.2
+ * Version: 2.11.3
  * Requires at least: 5.0
  * Requires PHP: 7.4
  * Text Domain: eazydocs
@@ -76,7 +76,7 @@ if ( ! class_exists( 'EazyDocs' ) ) {
 	class EazyDocs {
 
 		// Default constants
-		const version = '2.11.2';
+		const version = '2.11.3';
 		public $plugin_path;
 		public $theme_dir_path;
 
